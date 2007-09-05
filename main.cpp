e1f20c8ab64b462d3b95edda77e71a4be2640a8c
Sebastian Schuberth <sschuberth@gmail.com>
2007-09-05 15:05:39 +0000

    Added header with some comments / an introduction.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+/*
+ * This is a foobar2000 0.9.x <http://www.foobar2000.org/> component to interface
+ * with CD Art Display 1.x <http://www.closetosoftware.com/?s=cdartdisplay>.
+ *
+ * It is an alternative to using foo_winamp_spam in that it does not simulate the
+ * Winamp 1.x API <http://www.oldversion.com/program.php?n=winamp> but the more
+ * powerful Helium Music Manager 2007 API <http://www.helium-music-manager.com/>.
+ *
+ * This version of the component is written against the API specification version
+ * 1.6 dated 2007-09-04.
+ */
+
 // Get rid of tons of warnings.
 #pragma warning(push,1)
 #include <foobar2000/SDK/foobar2000.h>
