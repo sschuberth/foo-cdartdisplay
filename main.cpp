721c70ef705964ce715777749d9140e62edba384
Sebastian Schuberth <sschuberth@gmail.com>
2007-09-07 11:59:46 +0000

    Added shuffle / repeat control.

--- main.cpp
+++ main.cpp
@@ -53,10 +53,10 @@ enum HeliumMessage {
     IPC_SHOW_PLAYER_WINDOW                     ,
     IPC_GET_PLAYER_STATE                       ,
     IPC_PLAYER_STATE_CHANGED_NOTIFICATION      , // Message to send to CAD.
-    IPC_AUTOENQUEUE_OPTIONS                    , // Ignored.
-    IPC_SET_REPEAT                             , // TODO
+    IPC_AUTOENQUEUE_OPTIONS                    ,
+    IPC_SET_REPEAT                             ,
     IPC_SHUTDOWN_NOTIFICATION                  , // Message to send to CAD.
-    IPC_GET_REPEAT                             , // TODO
+    IPC_GET_REPEAT                             ,
     IPC_CLOSE_HELIUM                           ,
 
     IPC_RATING_CHANGED_NOTIFICATION       = 639, // Message to send to CAD.
@@ -69,6 +69,40 @@ enum HeliumState {
     HS_PLAYING    ,
     HS_PAUSED
 };
+
+// "Default" {BFC61179-49AD-4E95-8D60-A22706485505}
+static GUID const ORDER_DEFAULT=
+{ 0xBFC61179, 0x49AD, 0x4E95, { 0x8D, 0x60, 0xA2, 0x27, 0x06, 0x48, 0x55, 0x05 } };
+
+// "Repeat (playlist)" {681CC6EA-60AE-4BF9-913B-BB5F4E864F2A}
+static GUID const ORDER_REPEAT_PLAYLIST=
+{ 0x681CC6EA, 0x60AE, 0x4BF9, { 0x91, 0x3B, 0xBB, 0x5F, 0x4E, 0x86, 0x4F, 0x2A } };
+
+// "Repeat (track)" {4BF4B280-0BB4-4DD0-8E84-37C3209C3DA2}
+static GUID const ORDER_REPEAT_TRACK=
+{ 0x4BF4B280, 0x0BB4, 0x4DD0, { 0x8E, 0x84, 0x37, 0xC3, 0x20, 0x9C, 0x3D, 0xA2 } };
+
+// "Shuffle (tracks)" {C5CF4A57-8C01-480C-B334-3619645ADA8B}
+static GUID const ORDER_SHUFFLE_TRACKS=
+{ 0xC5CF4A57, 0x8C01, 0x480C, { 0xB3, 0x34, 0x36, 0x19, 0x64, 0x5A, 0xDA, 0x8B } };
+
+// "Shuffle (albums)" {499E0B08-C887-48C1-9CCA-27377C8BFD30}
+static GUID const ORDER_SHUFFLE_ALBUMS=
+{ 0x499E0B08, 0xC887, 0x48C1, { 0x9C, 0xCA, 0x27, 0x37, 0x7C, 0x8B, 0xFD, 0x30 } };
+
+// "Shuffle (directories)" {83C37600-D725-4727-B53C-BDEFFE5F8DC7}
+static GUID const ORDER_SHUFFLE_DIRECTORIES=
+{ 0x83C37600, 0xD725, 0x4727, { 0xB5, 0x3C, 0xBD, 0xEF, 0xFE, 0x5F, 0x8D, 0xC7 } };
+
+static t_size get_playback_order_index(static_api_ptr_t<playlist_manager>& plm,GUID const& guid)
+{
+    for (t_size i=0;i<plm->playback_order_get_count();++i) {
+        if (plm->playback_order_get_guid(i)==guid) {
+            return i;
+        }
+    }
+    return infinite;
+}
 
 class CDArtDisplayInterface:public initquit,public play_callback
 {
@@ -461,6 +495,48 @@ LRESULT CALLBACK CDArtDisplayInterface::WindowProc(HWND hWnd,UINT uMsg,WPARAM wP
                     return HS_PLAYING;
                 }
                 return HS_STOPPED;
+            }
+
+            case IPC_AUTOENQUEUE_OPTIONS: {
+                GUID guid=plm->playback_order_get_guid(plm->playback_order_get_active());
+
+                // Cycle through all "Shuffle" modes and "Default".
+                if (guid==ORDER_SHUFFLE_TRACKS) {
+                    plm->playback_order_set_active(get_playback_order_index(plm,ORDER_SHUFFLE_ALBUMS));
+                }
+                else if (guid==ORDER_SHUFFLE_ALBUMS) {
+                    plm->playback_order_set_active(get_playback_order_index(plm,ORDER_SHUFFLE_DIRECTORIES));
+                }
+                else if (guid==ORDER_SHUFFLE_DIRECTORIES) {
+                    plm->playback_order_set_active(get_playback_order_index(plm,ORDER_DEFAULT));
+                }
+                else {
+                    plm->playback_order_set_active(get_playback_order_index(plm,ORDER_SHUFFLE_TRACKS));
+                }
+
+                return 1;
+            }
+
+            case IPC_SET_REPEAT: {
+                GUID guid=plm->playback_order_get_guid(plm->playback_order_get_active());
+
+                // Cycle through all "Repeat" modes and "Default".
+                if (guid==ORDER_REPEAT_PLAYLIST) {
+                    plm->playback_order_set_active(get_playback_order_index(plm,ORDER_REPEAT_TRACK));
+                }
+                else if (guid==ORDER_REPEAT_TRACK) {
+                    plm->playback_order_set_active(get_playback_order_index(plm,ORDER_DEFAULT));
+                }
+                else {
+                    plm->playback_order_set_active(get_playback_order_index(plm,ORDER_REPEAT_PLAYLIST));
+                }
+
+                return 1;
+            }
+            case IPC_GET_REPEAT: {
+                // Check if the current order mode is any of the "Repeat" modes.
+                GUID guid=plm->playback_order_get_guid(plm->playback_order_get_active());
+                return guid==ORDER_REPEAT_PLAYLIST || guid==ORDER_REPEAT_TRACK;
             }
 
             case IPC_CLOSE_HELIUM: {
