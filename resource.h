93d52dd2d7cea792ec901b4a1edac26152246fae
Sebastian Schuberth <sschuberth@gmail.com>
2007-09-08 15:57:17 +0000

    Added missing GUI controls.

--- resource.h
+++ resource.h
@@ -2,9 +2,11 @@
 // Microsoft Visual C++ generated include file.
 // Used by preferences.rc
 //
-#define IDD_DIALOGBAR                   103
 #define IDD_CONFIG                      103
-#define IDC_CHECK1                      1001
+#define IDC_CAD_START                   1001
+#define IDC_CAD_PATH                    1002
+#define IDC_COVER_LABEL                 1003
+#define IDC_COVER_PATH                  1004
 
 // Next default values for new objects
 // 
@@ -12,7 +14,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
